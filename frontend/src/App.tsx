11b8d98ac5b964cdbfae5afd590016232a2693ec
Michał Marciniak <michal.mar725@gmail.com>
2026-01-24 12:45:34 +0100

    Fix: main page buttons not registering clicks

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,16 @@
 import { Button, Flex } from "antd";
-import { Link } from "react-router";
+import { useNavigate } from "react-router";
 
 function App() {
+  const navigate = useNavigate();
 
   return (
     <Flex style={{height: "100vh"}} justify="center" align="center">
       <Flex vertical gap="large" justify="center" align="center">
-        <Button type="default"><Link to="/login">Login</Link></Button>
-        <Button type="default"><Link to="/register">Register</Link></Button>
-        <Button type="default"><Link to="/send">Send message</Link></Button>
-        <Button type="default"><Link to="/inbox">Inbox</Link></Button>
+        <Button type="default" onClick={() => navigate('/login')}>Login</Button>
+        <Button type="default" onClick={() => navigate('/register')}>Register</Button>
+        <Button type="default" onClick={() => navigate('/send')}>Send message</Button>
+        <Button type="default" onClick={() => navigate('/inbox')}>Inbox</Button>
       </Flex>
     </Flex>
   )
